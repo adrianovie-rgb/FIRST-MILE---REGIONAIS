8caa729688590137b88e76d2f90c8720c054ee14
adrianovie-rgb <adriano_vie@hotmail.com>
2026-05-22 19:25:55 -0300

    Atualização automática Fri May 22 2026 00:00:00 GMT-0300 (Brasilia Standard Time) Sat Dec 30 1899 19:25:00 GMT-0306 (Brasilia Standard Time)

---
+++ data.js
@@ -0,0 +1,302 @@
+var D = {
+  "lastUpdate": "Fri May 22 2026 00:00:00 GMT-0300 (Brasilia Standard Time) Sat Dec 30 1899 19:25:00 GMT-0306 (Brasilia Standard Time)",
+  "meta": {
+    "ppp": 175,
+    "recv": 98.5,
+    "stow": 99.5,
+    "dep": 99.5,
+    "tph": 58.96
+  },
+  "weeks": [
+    "W15",
+    "W16",
+    "W17",
+    "W18",
+    "W19",
+    "W20"
+  ],
+  "feriados": {
+    "W15": "Sexta-feira Santa (11/04)",
+    "W17": "Tiradentes (21/04)",
+    "W18": "Dia do Trabalhador (01/05)"
+  },
+  "regionais": {
+    "DF-SUL": {
+      "vol": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ],
+      "ppp": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ],
+      "recv": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ],
+      "stow": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ],
+      "dep": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ]
+    },
+    "RIMES-NE": {
+      "vol": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ],
+      "ppp": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ],
+      "recv": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ],
+      "stow": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ],
+      "dep": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ]
+    },
+    "SP CAP": {
+      "vol": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ],
+      "ppp": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ],
+      "recv": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ],
+      "stow": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ],
+      "dep": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ]
+    },
+    "SP INT": {
+      "vol": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ],
+      "ppp": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ],
+      "recv": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ],
+      "stow": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ],
+      "dep": [
+        null,
+        null,
+        null,
+        null,
+        null,
+        null
+      ]
+    }
+  },
+  "ppp_comparativo": {
+    "FM": [
+      null,
+      null,
+      null,
+      null,
+      null,
+      null
+    ],
+    "DF-SUL": [
+      null,
+      null,
+      null,
+      null,
+      null,
+      null
+    ],
+    "SP CAP": [
+      null,
+      null,
+      null,
+      null,
+      null,
+      null
+    ],
+    "SP INT": [
+      null,
+      null,
+      null,
+      null,
+      null,
+      null
+    ],
+    "RIMES-NE": [
+      null,
+      null,
+      null,
+      null,
+      null,
+      null
+    ]
+  },
+  "tph": {
+    "ECB8": {
+      "target": null,
+      "w_anterior": null,
+      "w_atual": null
+    },
+    "EGO8": {
+      "target": null,
+      "w_anterior": null,
+      "w_atual": null
+    },
+    "ERS8": {
+      "target": null,
+      "w_anterior": null,
+      "w_atual": null
+    },
+    "ESB8": {
+      "target": null,
+      "w_anterior": null,
+      "w_atual": null
+    },
+    "ESE8": {
+      "target": null,
+      "w_anterior": null,
+      "w_atual": null
+    },
+    "EIJ8": {
+      "target": null,
+      "w_anterior": null,
+      "w_atual": null
+    },
+    "EJO8": {
+      "target": null,
+      "w_anterior": null,
+      "w_atual": null
+    },
+    "ELO8": {
+      "target": null,
+      "w_anterior": null,
+      "w_atual": null
+    },
+    "EME8": {
+      "target": null,
+      "w_anterior": null,
+      "w_atual": null
+    },
+    "EMR8": {
+      "target": null,
+      "w_anterior": null,
+      "w_atual": null
+    },
+    "EOZ8": {
+      "target": null,
+      "w_anterior": null,
+      "w_atual": null
+    },
+    "EUM8": {
+      "target": null,
+      "w_anterior": null,
+      "w_atual": null
+    },
+    "EVE8": {
+      "target": null,
+      "w_anterior": null,
+      "w_atual": null
+    }
+  }
+};
