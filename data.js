d20e2482d019542716ab65eb0c8161f55b2bf4ad
adrianovie-rgb <adriano_vie@hotmail.com>
2026-05-25 16:26:57 -0300

    Atualizacao data.js

--- data.js
+++ data.js
@@ -1,15 +1,15 @@
-var WK=["W16","W17","W18","W19","W20","W21"];
-var NW=6;
+var WK=["W15","W16","W17","W18","W19","W20","W21"];
+var NW=7;
 var R=["First Mile","DF-SUL","SP CAP","SP INT","RIMES-NE"];
 var R_THP=["First Mile","DF-SUL","SP CAP","RIMES-NE"];
 var C={"First Mile":"#232f3e","DF-SUL":"#2ecc71","SP CAP":"#3498db","SP INT":"#9b59b6","RIMES-NE":"#e67e22"};
 var M={ppp:175,recv:98.5,stow:99.5};
 var D={
-  ppp:{"First Mile":[null,null,null,null,null,null],"DF-SUL":[null,null,null,null,null,null],"SP CAP":[null,null,null,null,null,null],"SP INT":[null,null,null,null,null,null],"RIMES-NE":[null,null,null,null,null,null]},
-  vol:{"First Mile":[0,0,0,0,0,0],"DF-SUL":[0,0,0,0,0,0],"SP CAP":[0,0,0,0,0,0],"SP INT":[0,0,0,0,0,0],"RIMES-NE":[0,0,0,0,0,0]},
-  recv:{"First Mile":[null,null,null,null,null,null],"DF-SUL":[null,null,null,null,null,null],"SP CAP":[null,null,null,null,null,null],"SP INT":[null,null,null,null,null,null],"RIMES-NE":[null,null,null,null,null,null]},
-  stow:{"First Mile":[null,null,null,null,null,null],"DF-SUL":[null,null,null,null,null,null],"SP CAP":[null,null,null,null,null,null],"SP INT":[null,null,null,null,null,null],"RIMES-NE":[null,null,null,null,null,null]},
-  thp:{"First Mile":[null,null,null,null,null,null],"DF-SUL":[null,null,null,null,null,null],"SP CAP":[null,null,null,null,null,null],"RIMES-NE":[null,null,null,null,null,null]},
-  thpVol:{"First Mile":[0,0,0,0,0,0],"DF-SUL":[0,0,0,0,0,0],"SP CAP":[0,0,0,0,0,0],"RIMES-NE":[0,0,0,0,0,0]},
-  thpHrs:{"First Mile":[0,0,0,0,0,0],"DF-SUL":[0,0,0,0,0,0],"SP CAP":[0,0,0,0,0,0],"RIMES-NE":[0,0,0,0,0,0]}
+  ppp:{"First Mile":[null,null,null,null,null,null,null],"DF-SUL":[null,null,null,null,null,null,null],"SP CAP":[null,null,null,null,null,null,null],"SP INT":[null,null,null,null,null,null,null],"RIMES-NE":[null,null,null,null,null,null,null]},
+  vol:{"First Mile":[0,0,0,0,0,0,0],"DF-SUL":[0,0,0,0,0,0,0],"SP CAP":[0,0,0,0,0,0,0],"SP INT":[0,0,0,0,0,0,0],"RIMES-NE":[0,0,0,0,0,0,0]},
+  recv:{"First Mile":[null,null,null,null,null,null,null],"DF-SUL":[null,null,null,null,null,null,null],"SP CAP":[null,null,null,null,null,null,null],"SP INT":[null,null,null,null,null,null,null],"RIMES-NE":[null,null,null,null,null,null,null]},
+  stow:{"First Mile":[null,null,null,null,null,null,null],"DF-SUL":[null,null,null,null,null,null,null],"SP CAP":[null,null,null,null,null,null,null],"SP INT":[null,null,null,null,null,null,null],"RIMES-NE":[null,null,null,null,null,null,null]},
+  thp:{"First Mile":[null,null,null,null,null,null,null],"DF-SUL":[null,null,null,null,null,null,null],"SP CAP":[null,null,null,null,null,null,null],"RIMES-NE":[null,null,null,null,null,null,null]},
+  thpVol:{"First Mile":[0,0,0,0,0,0,0],"DF-SUL":[0,0,0,0,0,0,0],"SP CAP":[0,0,0,0,0,0,0],"RIMES-NE":[0,0,0,0,0,0,0]},
+  thpHrs:{"First Mile":[0,0,0,0,0,0,0],"DF-SUL":[0,0,0,0,0,0,0],"SP CAP":[0,0,0,0,0,0,0],"RIMES-NE":[0,0,0,0,0,0,0]}
 };
