184cdbcf24638b54dd49078aa90150a963830a16
adrianovie-rgb <adriano_vie@hotmail.com>
2026-05-27 16:01:23 -0300

    Atualizacao data.js

--- data.js
+++ data.js
@@ -3,7 +3,7 @@ var D={
   regionals:["First Mile","DF-SUL","SP CAP","SP INT","RIMES-NE"],
   colors:{"First Mile":"#2c3e50","DF-SUL":"#9b59b6","SP CAP":"#95a5a6","SP INT":"#e67e22","RIMES-NE":"#27ae60"},
   meta:{ppp:175,recv:98.5,stow:99.5},
-  lastUpdate:"27/05/2026 13:49",
+  lastUpdate:"27/05/2026 16:01",
   ppp:{"First Mile":[0],"DF-SUL":[0],"SP CAP":[0],"SP INT":[0],"RIMES-NE":[0]},
   recv:{"First Mile":[0],"DF-SUL":[0],"SP CAP":[0],"SP INT":[0],"RIMES-NE":[0]},
   stow:{"First Mile":[0],"DF-SUL":[0],"SP CAP":[0],"SP INT":[0],"RIMES-NE":[0]},
