ca0c056aa00660528b5f82d1ba5d198003ac1bb3
adrianovie-rgb <adriano_vie@hotmail.com>
2026-05-28 02:41:17 -0300

    Atualizacao data.js

--- data.js
+++ data.js
@@ -3,6 +3,6 @@ var D={
   regionals:["First Mile","DF-SUL","SP CAP","SP INT","RIMES-NE"],
   colors:{"First Mile":"#2c3e50","DF-SUL":"#9b59b6"},
   meta:{ppp:175,recv:98.5,stow:99.5},
-  lastUpdate:"28/05/2026 02:30",
+  lastUpdate:"28/05/2026 02:41",
   labelConfig: {"showAll": true, "collisionDetection": true},
 };
